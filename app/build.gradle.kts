ccb32a65b04b0a50dde5f10570e79a65f0a37c6a
MiguelCMayo <miguelcmayo@gmail.com>
2024-07-04 22:20:34 +0200

    add-dedekt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.gradle.ktlint)
+    alias(libs.plugins.detekt)
 }
 
 android {
